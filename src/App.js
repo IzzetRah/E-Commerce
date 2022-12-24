68aafc378dcb0359996d82a75e44f3cd935df3e3
IzzetRah <115147778+IzzetRah@users.noreply.github.com>
2022-12-24 17:35:32 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './App.css'
+import Header from './common/header/Header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Pages from './pages/Pages'
+import Data from './components/flashDeals/Data'
+const App = () => {
+  // step 1: fetch data from database
+  const { productItems } = Data
+  const [cartItem, setCartItem] = useState([])
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Pages productItems={productItems} />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  )
+}
+export default App
